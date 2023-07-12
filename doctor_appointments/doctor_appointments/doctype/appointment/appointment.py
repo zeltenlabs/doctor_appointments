8fc06b4399b79dee56b5f5dbe5a72c1403bcc428
parrasajad <parrasajad@gmail.com>
2023-07-12 20:27:29 +0530

    add session, session item, add appointment automatically to related session

--- doctor_appointments/doctor_appointments/doctype/appointment/appointment.py
+++ doctor_appointments/doctor_appointments/doctype/appointment/appointment.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2023, ZeltenLabs and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Appointment(Document):
-	pass
+	def after_insert(self):
+		# add the appointment to the session as a new session item
+		print("your queue number:",self.add_to_session())
+	def add_to_session(self):
+		session = frappe.get_doc("Session", {"date": self.date, "shift": self.shift, "clinic": self.clinic})
+
+		session.append("queue", {
+			"appointment": self.name,
+			"status": "Pending"
+		})
+		session.save(ignore_permissions=True)
+
+		return len(session.queue)
