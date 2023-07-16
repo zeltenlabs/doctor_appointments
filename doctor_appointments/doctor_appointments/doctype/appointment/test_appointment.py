0c233b8168711a26211bbf8fd71276cfbb3edc3f
parrasajad <parrasajad@gmail.com>
2023-07-16 13:10:28 +0530

    start tdd

--- doctor_appointments/doctor_appointments/doctype/appointment/test_appointment.py
+++ doctor_appointments/doctor_appointments/doctype/appointment/test_appointment.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2023, ZeltenLabs and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestAppointment(FrappeTestCase):
-	pass
+	def test_add_to_session(self):
+		doctor = frappe.get_doc({"doctype": "Doctor", "first_name": "Test Doctor", "speciality": "Gastro"}).insert()
+		clinic = frappe.get_doc({"doctype": "Clinic", "name": "Test Clinic", "contact_number": "999"}).insert()
+		shift = frappe.get_doc({"doctype": "Schedule Shift", "start_time": "9:00:00","end_time": "15:00:00", "clinic": clinic.name}).insert()
+		appointment = frappe.get_doc({
+			"doctype": "Appointment",
+			"clinic": clinic.name,
+			"shift": shift.name,
+			"date": "202-08-01",
+			"patient_name": "Test Patient",
+		}).insert()
+
+	def tearDown(self):
+		frappe.db.rollback()
